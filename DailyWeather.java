4cd663feb1e1ae67852fd006a0eff631aaa6402b
mthoffma <63079783+mthoffma@users.noreply.github.com>
2020-04-06 09:15:16 -0400

    Update DailyWeather.java

--- DailyWeather.java
+++ DailyWeather.java
@@ -1,5 +1,6 @@
-/**
- * this class creates a DailyWeather object that will store 
+
+ /**
+ * This class creates a DailyWeather object that will store 
  * characteristics for weather on a daily basis. This will be
  * used in conjunction with the WeatherAPI Class.
  *
@@ -13,7 +14,16 @@ public class DailyWeather {
 	double precipitation;
 	double snow;
 	
-	
+	/**
+	 * Constructor for DailyWeather
+	 * @param date
+	 * @param station
+	 * @param zipCode
+	 * @param max
+	 * @param min
+	 * @param precip
+	 * @param snowfall
+	 */
 	public DailyWeather(String date, String station, String zipCode, double max, double min, double precip, double snowfall) {
 		super();
 		this.date = date;
@@ -26,59 +36,114 @@ public class DailyWeather {
 	}
 	
 
-
+	/**
+	 * 
+	 * @return date
+	 */
 	public String getDate() {
 		return date;
 	}
 
+	/**
+	 * 
+	 * @param date
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
+	/**
+	 * 
+	 * @return max temp
+	 */
 	public double getTempMax() {
 		return tempMax;
 	}
 
+	/**
+	 * 
+	 * @param tempMax
+	 */
 	public void setTempMax(double tempMax) {
 		this.tempMax = tempMax;
 	}
 
+	/**
+	 * 
+	 * @return min temp
+	 */
 	public double getTempMin() {
 		return tempMin;
 	}
 
+	/**
+	 * 
+	 * @param tempMin
+	 */
 	public void setTempMin(double tempMin) {
 		this.tempMin = tempMin;
 	}
 
+	/**
+	 * 
+	 * @return station
+	 */
 	public String getStation() {
 		return station;
 	}
 
+	/**
+	 * 
+	 * @param station
+	 */
 	public void setStation(String station) {
 		this.station = station;
 	}
 
+	/**
+	 * 
+	 * @return ZIP code
+	 */
 	public String getZipCode() {
 		return zipCode;
 	}
 
+	/**
+	 * 
+	 * @param zipCode
+	 */
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}	
 	
+	/**
+	 * 
+	 * @return preciitation
+	 */
 	public double getPrecipitation() {
 		return precipitation;
 	}
 
+	/**
+	 * 
+	 * @param precipitation
+	 */
 	public void setPrecipitation(double precipitation) {
 		this.precipitation = precipitation;
 	}
 
+	/**
+	 * 
+	 * @return snow
+	 */
 	public double getSnow() {
 		return snow;
 	}
 
+	/**
+	 * 
+	 * @param snow
+	 */
 	public void setSnow(double snow) {
 		this.snow = snow;
 	}
