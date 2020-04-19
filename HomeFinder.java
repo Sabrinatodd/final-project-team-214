2f2ee5ee20ebf480f78c8d4ba7575983dccb7091
joconn <joconn@seas.upenn.edu>
2020-04-19 13:12:00 -0700

    Updated to work with new DataReader class

--- HomeFinder.java
+++ HomeFinder.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 /**
  * This class represents the main class used for our final
@@ -14,15 +15,14 @@ public class HomeFinder {
 	 */
 	public void run() {
 		
-		//Read home value data from Zillow
-		
-		//Call Weather API
-		
-		//Clean Data and Insert into ZipCode Class
+		//Read and clean all data to be analyzed
+		DataReader reader = new DataReader();
+		HashMap<String, DataBook> allLocationData = reader.readAllData();
 		
 		//Use GUI to get User Input on desirable home characteristics
 		
 		//Parse through data and score ZipCodes based on User Input Match
+		HomeMatchScorer scorer = new HomeMatchScorer(allLocationData);
 		
 		//Rank ZipCodes by their assigned score
 		
--- HomeMatchScorer.java
+++ HomeMatchScorer.java
@@ -3,38 +3,42 @@ import java.util.*;
 /**
  * This class will be used  to take user input on their home
  * preferences and score 
- *
  */
 public class HomeMatchScorer {
+	HashMap<String, DataBook> hashOfAllCleanedData;
+	
+	public HomeMatchScorer(HashMap<String, DataBook> hashOfAllDataByZip) {
+		this.hashOfAllCleanedData = hashOfAllDataByZip;
+	}
 	
 	/**
 	 * this method provides a score based on the user's preferences
 	 * for the home price and number of bedrooms in a Zip Code
 	 * @return double representing the score of a zip code based on the user's preferred home price
 	 */
-	public double scoreHomeValue(ArrayList<ZipCode> cleanedHomeValueData, int userPreferredBedrooms, long userBudget) {
-		
-		//for each zip code test to see if home price value is below the 
-		//user's budget given the number of bedrooms they wish to have
+	public HashMap<String, Double> scoreHomeValue(int userPreferredBedrooms, long userBudget) {
+		HashMap<String, Double> homeScores = new HashMap<String, Double>();
 		
 		//take all Zip Codes that fall below budget and determine how much
-		//lower than the budget the average home price is. A large
-		//difference will be scored more highly because it demonstrates
-		//a saving opportunity for the buyers.
-		int nCount = 0;
-		int total = 0;
-		
-		for( ZipCode homeData :cleanedHomeValueData){
-			int homePrice = homeData.getPriceByRooms(userPreferredBedrooms);
-			if(homePrice <= userBudget){
-				total += homePrice;
-				nCount++;
+		//lower than the budget the average home price is
+		for(String zipCode : hashOfAllCleanedData.keySet()) {
+			DataBook allData = hashOfAllCleanedData.get(zipCode);
+			int homePrice = allData.getPriceByRooms(userPreferredBedrooms);
+			double score = 0.0;
+			
+	
+			if(homePrice <= userBudget) {
+				//When all other scores are equal, we would like to score
+				//lower cost zip codes (i.e. those with a larger difference
+				//from the user's budget) more highly because it offers 
+				//an additional saving opportunity for buyers
+				score = 1 - (homePrice / userBudget);
 			}
+			
+			homeScores.put(zipCode, score);
 		}
-		double averageHomePrice  =  total / nCount;
-		// Add score logic
 		
-		return averageHomePrice;
+		return homeScores;
 	}
 	
 	
@@ -106,7 +110,7 @@ public class HomeMatchScorer {
 	 * @param precipitationScore the score assigned to the zip code based on the user's preferred precipitation levels
 	 * @return total score across all factors for a given zip code
 	 */
-	public double totalAreaScore(ZipCode zip, double homeValueScore, double temperatureScore, double precipitationScore) {
+	public double totalAreaScore(DataBook zip, double homeValueScore, double temperatureScore, double precipitationScore) {
 		//take an average of the home value score, temperature score, and
 		//precipitation score to provide an overall score for a zip code
 		return (homeValueScore + temperatureScore + precipitationScore)/ 3;
@@ -119,7 +123,7 @@ public class HomeMatchScorer {
 	 * @param allData HashMap of all data for each Zip Code
 	 * @return HashMap that maps and ranks overall scores to each Zip Code in CA
 	 */
-	public HashMap<String, Double> topZipCodes(HashMap<String, ZipCode> allData, ArrayList<DailyWeather>cleanedWeatherData){
+	public HashMap<String, Double> topZipCodes(HashMap<String, DataBook> allData, ArrayList<DailyWeather>cleanedWeatherData){
 		//for each zip code in provided hashmap, collect an totalAreaScore
 		
 		//add Zip Code and total score to HashMap to be returned
@@ -130,7 +134,8 @@ public class HomeMatchScorer {
 		
 		HashMap<String, Double>  scoreMap = new HashMap<>();
 		for(String zipcode : allData.keySet()){
-			double totalScore = totalAreaScore(allData.get(zipcode).getHomePriceOverall() , cleanedWeatherData.get(index) , precipitationScore)
+			double totalScore = totalAreaScore(allData.get(zipcode).getHomePriceOverall() , cleanedWeatherData.get(index) , precipitationScore);
 		}
 	}
 }
+
