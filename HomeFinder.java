70ea2cf4b33f91d74d1284ff4ee509e2335839de
mthoffma <63079783+mthoffma@users.noreply.github.com>
2020-04-25 09:37:27 -0400

    Update HomeFinder.java

--- HomeFinder.java
+++ HomeFinder.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 /**
  * This class represents the main class used for our final
  * project. This class will use the data and user input
@@ -14,18 +12,10 @@ public class HomeFinder {
 	 * running and utilizing all of the other methods we create
 	 */
 	public void run() {
-		
-		//Read and clean all data to be analyzed
-		DataReader reader = new DataReader();
-		HashMap<String, DataBook> allLocationData = reader.readAllData();
-		
-		//Use GUI to get User Input on desirable home characteristics
-		
-		//Parse through data and score ZipCodes based on User Input Match
-		HomeMatchScorer scorer = new HomeMatchScorer(allLocationData);
-		
-		//Rank ZipCodes by their assigned score
-		
-		//Output ZipCodes to User
+		 java.awt.EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                new HousingGUI().setVisible(true);
+	            }
+	        });
 	}
 }
